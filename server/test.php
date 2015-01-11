b03fec70acbbe2f37da2f7f35e27737f4aabed4b
ngoluan <luan.t.ngo@gmail.com>
2015-01-11 14:07:44 -0500

    0111

--- server/test.php
+++
@@ -1,9 +0,0 @@
-<?
-
-$files = scandir('uploads/luan.t.ngo@gmail.com');
-$files = array_diff( $files, array(".", ".."));
-foreach($files as $file) {
-	echo $file . "<br>";
-	unlink("uploads/luan.t.ngo@gmail.com/".$file);
-}
-?>
